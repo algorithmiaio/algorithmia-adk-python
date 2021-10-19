2521309433395a37ca24fb920b4a9e136689e4f3
James Sutton <1892175+zeryx@users.noreply.github.com>
2021-10-19 17:14:10 -0400

    restructured dir structure

--- adk/ADK.py
+++ adk/ADK.py
@@ -4,7 +4,7 @@ import os
 import sys
 import Algorithmia
 from adk.io import create_exception, format_data, format_response
-from adk.manifest import ModelData
+from adk.modeldata import ModelData
 
 
 class ADK(object):
